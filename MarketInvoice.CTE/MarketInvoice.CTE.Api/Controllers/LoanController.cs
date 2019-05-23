485d997ebb02824012f3b6cea0349aad085e956c
Dao Lam <daolavi@gmail.com>
2019-05-23 19:41:36 +0700

    Init solution

---
+++ MarketInvoice.CTE/MarketInvoice.CTE.Api/Controllers/LoanController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketInvoice.CTE.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoanController
+    {
+        public LoanController()
+        {
+        }
+
+        [HttpGet]
+        [Route("loansummary")]
+        public IActionResult LoanSummary()
+        {
+            return new JsonResult("Ok");
+        }
+
+        [HttpGet]
+        [Route("repaymentSchedule")]
+        public IActionResult RepaymentSchedule()
+        {
+            return new JsonResult("Ok");
+        }
+    }
+}
