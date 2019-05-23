a21b6668045134b7028befcc2a254aa28f7e3c65
Dao Lam <daolavi@gmail.com>
2019-05-23 21:12:40 +0700

    Validate input

--- MarketInvoice.CTE/MarketInvoice.CTE.Models/Request/LoanInformation.cs
+++ MarketInvoice.CTE/MarketInvoice.CTE.Models/Request/LoanInformation.cs
@@ -1,17 +1,22 @@
 ﻿using MarketInvoice.CTE.Constants;
+using MarketInvoice.CTE.Validations;
 using Newtonsoft.Json;
+
 
 namespace MarketInvoice.CTE.Models.Request
 {
     public class LoanInformation
     {
         [JsonProperty("amount")]
+        [RequiredGreaterThanZero]
         public decimal Amount { get; set; }
-
+        
         [JsonProperty("apr")]
+        [RequiredGreaterThanZero]
         public decimal APR { get; set; }
 
         [JsonProperty("numberOfPayments")]
+        [RequiredGreaterThanZero]
         public int NumberOfPayments { get; set; } = PaymentConstants.WEEKLY;
     }
 }
---
+++ MarketInvoice.CTE/MarketInvoice.CTE.Validations/RequiredGreaterThanZero.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketInvoice.CTE.Validations
+{
+    public class RequiredGreaterThanZero : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is decimal)
+            {
+                return value != null && decimal.TryParse(value.ToString(), out var i) && i > 0;
+            }
+            else if (value is int)
+            {
+                return value != null && int.TryParse(value.ToString(), out var i) && i > 0;
+            }
+
+            return false;
+        }
+    }
+}
